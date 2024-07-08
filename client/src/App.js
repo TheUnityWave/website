ea5b3ae2f273d9248e26861ce2195a1f407d3b7b
Yash Dave <yashdave307@gmail.com>
2024-07-08 14:28:23 +0530

    completed whyUs section

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './Components/Navbar'
+import WhyUs from './Components/WhyUs'
 
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
+      <WhyUs />
       
     </Router>
   )
