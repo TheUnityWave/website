44cb9106664c4d2f35d4eca95330f10eef66daf2
Yash Dave <yashdave307@gmail.com>
2024-07-09 17:17:42 +0530

    careers page completed

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import WhyUs from './Components/WhyUs'
+import Careers from './Pages/Careers'
 
 
 function App() {
   return (
     <Router>
-      {/* <Navbar /> */}
-      <WhyUs />
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<WhyUs />} />
+        <Route path="/career" element={<Careers />} />
+      </Routes>
     </Router>
   )
 }
