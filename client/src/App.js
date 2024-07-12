4a8373361212f71e200acfcfaf00fbf20fcc9ac6
RchtDshr <rachita17dashore@gmail.com>
2024-07-12 15:23:39 +0530

    fixed navigation across pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { useEffect, useCallback } from 'react'
+import { BrowserRouter, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import WhyUs from './Components/WhyUs'
 import Careers from './Pages/Careers'
@@ -9,28 +9,76 @@ import GetInTouch from './Pages/GetInTouch'
 import Services from './Pages/Services'
 import ServiceDetails from './Pages/ServiceDetails'
 
+function ScrollManager() {
+  const { pathname, hash } = useLocation();
+  const navigate = useNavigate();
+
+  const scrollWithOffset = useCallback((elementId, offset = 100) => {
+    const element = document.getElementById(elementId);
+    if (element) {
+      const elementPosition = element.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+      return true;
+    }
+    return false;
+  }, []);
+
+  useEffect(() => {
+    if (hash) {
+      setTimeout(() => {
+        scrollWithOffset(hash.slice(1));
+      }, 100);
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash, scrollWithOffset]);
+
+  // Expose a function to handle scrolling
+  window.scrollToSection = (sectionId) => {
+    if (sectionId === 'getintouch') {
+      // Try to scroll to getintouch on the current page
+      if (!scrollWithOffset(sectionId)) {
+        // If getintouch doesn't exist on the current page, navigate to home
+        navigate(`/#${sectionId}`);
+      }
+    } else if (pathname === '/') {
+      scrollWithOffset(sectionId);
+    } else {
+      navigate(`/#${sectionId}`);
+    }
+  };
+
+  return null;
+}
+
+
+
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-
-      <div className='relative top-24 font-primary'>
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Stats />
-              <Services />
-              <WhyUs />
-              <GetInTouch/>
-            </>
-          } />
-          <Route path="/career" element={<Careers />} />
-          <Route path="/service/:serviceId" element={<ServiceDetails/>} />
-        </Routes>
-          <Footer />
-      </div>
-
-    </BrowserRouter>
+    <ScrollManager />
+    <Navbar />
+    <div className='relative top-24 font-primary'>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Stats />
+            <Services />
+            <WhyUs />
+          </>
+        } />
+        <Route path="/career" element={<Careers />} />
+        <Route path="/service/:serviceId" element={<ServiceDetails/>} />
+      </Routes>
+      <GetInTouch/>
+      <Footer />
+    </div>
+  </BrowserRouter>
   )
 }
 
