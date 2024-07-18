9977aafd47d94263795a6cd30ff887989fb831b7
Yash Dave <yashdave307@gmail.com>
2024-07-18 21:25:23 +0530

    mobile fix

--- server/models/getintouch.js
+++ server/models/getintouch.js
@@ -13,7 +13,7 @@ const GetInTouchSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    mobileNumber: {
+    mobile: {
         type: String, 
         required: true
     },
