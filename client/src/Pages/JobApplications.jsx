ff1ccd075d6fafc944b7f523c32269b336ae7fe7
RchtDshr <rachita17dashore@gmail.com>
2024-07-23 12:57:05 +0530

    fixed fetching error

--- client/src/Pages/JobApplications.jsx
+++ client/src/Pages/JobApplications.jsx
@@ -17,7 +17,8 @@ const JobApplications = () => {
         };
         fetchApplications();
 
-    }, [applications]);
+    }, [setApplications]);
+
     const sendCredentials = async (id) => {
         try {
             const response = await fetch(`http://localhost:5000/api/admin/send-credentials/${id}`, {
