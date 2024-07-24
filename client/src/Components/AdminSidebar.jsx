dd08db195cabd9fc376b94b3197da928201b3806
Yash Dave <yashdave307@gmail.com>
2024-07-24 14:38:44 +0530

    admin dashboard responsive

--- client/src/Components/AdminSidebar.jsx
+++ client/src/Components/AdminSidebar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import ChangePasswordModal from './ChangePasswordModal';
+import { ClipboardList, BookUser, PhoneCall } from 'lucide-react';
 
 const AdminSidebar = () => {
     const [isChangePasswordOpen, setIsChangePasswordOpen] = useState(false);
@@ -18,30 +19,48 @@ const AdminSidebar = () => {
     }
 
     return (
-        <div className="bg-cyan-900 text-white w-80 p-4 flex flex-col justify-between">
-            <nav className="flex flex-col space-y-4">
-                <NavLink to="/admin/job-applications" className="hover:bg-cyan-700 p-2 rounded">
+        <>
+            <div className="hidden lg:flex bg-cyan-900 text-white w-80 p-4 flex flex-col justify-between">
+                <nav className="flex flex-col space-y-4">
+                    <NavLink to="/admin/job-applications" className="hover:bg-cyan-700 p-2 rounded">
+                        Job Applications
+                    </NavLink>
+                    <NavLink to="/admin/employees" className="hover:bg-cyan-700 p-2 rounded">
+                        Employees Detail
+                    </NavLink>
+                    <NavLink to="/admin/getintouch" className="hover:bg-cyan-700 p-2 rounded">
+                        Get in Touch Requests
+                    </NavLink>
+                </nav>
+                <nav className='flex flex-col'>
+                    <NavLink onClick={toggleChangePassword} className="hover:bg-cyan-700 p-2 rounded mt-4">
+                        Change Password
+                    </NavLink>
+                    <NavLink onClick={handleLogout} to="/" className="hover:bg-cyan-700 p-2 rounded mt-4">
+                        Logout
+                    </NavLink>
+                </nav>
+                {isChangePasswordOpen && (
+                    <ChangePasswordModal onClose={toggleChangePassword} />
+                )}
+            </div>
+
+            {/* Bottom Navbar for Small Devices */}
+            <div className="lg:hidden fixed bottom-0 left-0 right-0 bg-cyan-900 text-white flex justify-around p-2">
+                <NavLink to="/admin/job-applications" className="hover:bg-cyan-700 p-2 rounded text-center">
+                    <ClipboardList className='m-auto' />
                     Job Applications
                 </NavLink>
-                <NavLink to="/admin/employees" className="hover:bg-cyan-700 p-2 rounded">
+                <NavLink to="/admin/employees" className="hover:bg-cyan-700 p-2 rounded text-center">
+                    <BookUser className='m-auto' />
                     Employees Detail
                 </NavLink>
-                <NavLink to="/admin/getintouch" className="hover:bg-cyan-700 p-2 rounded">
+                <NavLink to="/admin/getintouch" className="hover:bg-cyan-700 p-2 rounded text-center">
+                    <PhoneCall className='m-auto' />
                     Get in Touch Requests
                 </NavLink>
-            </nav>
-            <nav className='flex flex-col'>
-                <NavLink onClick={toggleChangePassword} className="hover:bg-cyan-700 p-2 rounded mt-4">
-                    Change Password
-                </NavLink>
-                <NavLink onClick={handleLogout} to="/" className="hover:bg-cyan-700 p-2 rounded mt-4">
-                    Logout
-                </NavLink>
-            </nav>
-            {isChangePasswordOpen && (
-                <ChangePasswordModal onClose={toggleChangePassword} />
-            )}
-        </div>
+            </div>
+        </>
     );
 };
 
