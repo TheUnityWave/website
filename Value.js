13d2d1731dfbb9dfdc25189c21bc98f983689325
sonali-afk <singhsonali2911@gmail.com>
2024-08-03 15:40:42 +0530

    Create Value.js

---
+++ Value.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Values = () => (
+  <div className="content values">
+    <h2>Our Values</h2>
+    <div className="background">
+      <p>At the core of our company are the values that drive us forward...</p>
+    </div>
+    <img src="https://www.cultureworkshr.com/wp-content/uploads/2022/09/iStock-1134456412-1030x687.jpg" alt="Our Values" />
+  </div>
+);
+
+export default Values;
