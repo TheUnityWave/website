0a3526d6b95fa174b42e49b2acfe105e773b991f
sonali-afk <singhsonali2911@gmail.com>
2024-08-03 16:38:37 +0530

    Update Mission.js

--- Mission.js
+++ Mission.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Mission = () => (
   <div className="content mission">
     <h2>Explore Our Mission</h2>
-    <p>Our mission is to deliver exceptional facility management services...</p>
+    <p>Our mission is to deliver comprehensive and innovative facility management solutions that enhance the functionality, safety, and sustainability of the environments we serve, while exceeding our clients' expectations through exceptional service and unwavering commitment to quality.</p>
     <img src="https://media.licdn.com/dms/image/D5612AQGF-nIfg1t2Rw/article-cover_image-shrink_600_2000/0/1682961964037?e=2147483647&v=beta&t=q_XFEcdnCRHqNB9aAT4ckKynlVXwSDW1755MM5bjzJI" alt="Our Mission" />
   </div>
 );
