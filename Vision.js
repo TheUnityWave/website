1b594631b24b7a15e0abee397a74a6bf25ec7cfb
sonali-afk <singhsonali2911@gmail.com>
2024-08-03 16:37:53 +0530

    Update Vision.js

--- Vision.js
+++ Vision.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Vision = () => (
   <div className="content vision">
     <h2>Discover Our Vision</h2>
-    <p>To be the leading provider of integrated facility management services...</p>
+    <p> To become the benchmark for excellence in facility management by delivering exceptional services that optimize space, enhance productivity, and contribute to a sustainable future.</p>
     <img src="https://img.freepik.com/free-vector/vision-statement-concept-illustration_114360-7576.jpg" alt="Our Vision" />
   </div>
 );
