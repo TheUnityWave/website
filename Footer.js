4aa199cccfef3002ab2eabd8e3268b3b233f62d9
sonali-afk <singhsonali2911@gmail.com>
2024-08-03 15:43:54 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <p>Doing Things Differently</p>
+    <p>Designed and developed by our Team</p>
+  </footer>
+);
+
+export default Footer;
