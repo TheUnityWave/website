90e1e02a38f64ca7da5d5331ebf34f19ac3c7c7c
sonali-afk <singhsonali2911@gmail.com>
2024-08-03 15:41:13 +0530

    Create Objectives.js

---
+++ Objectives.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Objectives = () => (
+  <div className="content objectives">
+    <h2>Our Objectives</h2>
+    <div className="background">
+      <p>Our primary objectives are to provide high-quality, reliable facility management services...</p>
+    </div>
+    <img src="https://img.freepik.com/free-photo/business-strategy-success-target-goals_1421-33.jpg" alt="Our Objectives" />
+  </div>
+);
+
+export default Objectives;
