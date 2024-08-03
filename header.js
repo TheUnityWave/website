0f257d75cdcbddcae65c2aa198dea72b36e60d81
sonali-afk <singhsonali2911@gmail.com>
2024-08-03 15:36:46 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="header">
+    <h1>About Us</h1>
+    <p className="tagline">Doing Things Differently</p>
+  </div>
+);
+
+export default Header;
