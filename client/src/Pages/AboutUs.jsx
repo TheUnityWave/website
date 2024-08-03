a2a1e287a229d770750a9b647c976e34a79d8e30
RchtDshr <rachita17dashore@gmail.com>
2024-08-03 18:00:57 +0530

    del unnecessary files

--- client/src/Pages/AboutUs.jsx
+++ client/src/Pages/AboutUs.jsx
@@ -64,42 +64,6 @@ const AboutUs = () => {
                     </div>
                 </div>
             </div>
-            {/* <div className="min-h-screen w-full bg-white flex flex-col items-center justify-between py-0"> */}
-            {/* <div className="flex-1 flex flex-col md:flex-row w-full p-8 bg-gray-50"> */}
-            {/* <div className="md:w-1/2 flex items-center justify-center">
-            <img src={image} alt="Illustration" className="w-full h-full object-contain" />
-        </div> */}
-            {/* <div className="md:w-1/2 pl-8 flex flex-col justify-center">
-            <h2 className="text-4xl font-bold mb-10">Why choose us?</h2>
-            <ul className="space-y-4 text-lg">
-            <li className="flex items-center">
-                <span className="text-green-600 mr-2">✔</span> Proactiveness
-            </li>
-            <li className="flex items-center">
-                <span className="text-green-600 mr-2">✔</span> Technology focus
-            </li>
-            <li className="flex items-center">
-                <span className="text-green-600 mr-2">✔</span> Client support
-            </li>
-            <li className="flex items-center">
-                <span className="text-green-600 mr-2">✔</span> Agility
-            </li>
-            <li className="flex items-center">
-                <span className="text-green-600 mr-2">✔</span> Fast driven solutions
-            </li>
-            </ul>
-        </div>
-        </div>
-        <div className="w-full p-6 bg-gray-200 flex items-center justify-bewteen py-8">
-        <h3 className="text-3xl font-bold border-r-5 border-black pr-4">We Proudly Serve</h3>
-        <div className="flex space-x-96 ml-10">
-            <img src={company1} alt="Company 1" className="h-12" />
-            <img src={company2} alt="Company 2" className="h-12" />
-            <img src={company3} alt="Company 3" className="h-12" />
-        </div> */}
-        {/* </div> */}
-        {/* // </div> */}
-            
     </div >
     )
 }
