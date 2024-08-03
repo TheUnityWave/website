b14ada25eaa5be35f5e5777bddf22689fcd56675
sonali-afk <singhsonali2911@gmail.com>
2024-08-03 15:43:24 +0530

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => (
+  <div className="content contact">
+    <h2>Contact Us</h2>
+    <p>For more information about our services or to discuss your facility management needs, please contact us at <strong>Rahul Arora</strong> or visit our website.</p>
+    <p>Phone: +91-7351087669</p>
+    <p>Email: arora.rahul1118@gmail.com</p>
+    <p>Location: C-201,Mahima Paronama,VIT Road,Japgatpura,Jaipur,Rajasthan,302017</p>
+  </div>
+);
+
+export default Contact;
