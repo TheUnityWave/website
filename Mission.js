9ef3c3e1a0db50d080cb7af616427b4ef1a3058c
sonali-afk <singhsonali2911@gmail.com>
2024-08-03 15:39:57 +0530

    Create Mission.js

---
+++ Mission.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mission = () => (
+  <div className="content mission">
+    <h2>Explore Our Mission</h2>
+    <p>Our mission is to deliver exceptional facility management services...</p>
+    <img src="https://media.licdn.com/dms/image/D5612AQGF-nIfg1t2Rw/article-cover_image-shrink_600_2000/0/1682961964037?e=2147483647&v=beta&t=q_XFEcdnCRHqNB9aAT4ckKynlVXwSDW1755MM5bjzJI" alt="Our Mission" />
+  </div>
+);
+
+export default Mission;
