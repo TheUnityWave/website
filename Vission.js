351885adc2b8c7aae8c5ac50aada5ba1a4cd911c
sonali-afk <singhsonali2911@gmail.com>
2024-08-03 15:37:58 +0530

    Create Vission.js

---
+++ Vission.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Vision = () => (
+  <div className="content vision">
+    <h2>Discover Our Vision</h2>
+    <p>To be the leading provider of integrated facility management services...</p>
+    <img src="https://img.freepik.com/free-vector/vision-statement-concept-illustration_114360-7576.jpg" alt="Our Vision" />
+  </div>
+);
+
+export default Vision;
