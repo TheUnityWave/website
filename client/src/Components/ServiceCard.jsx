67788df92276bd05de5a1ee1845732424ef8bbbd
RchtDshr <rachita17dashore@gmail.com>
2024-08-10 22:57:21 +0530

    service content

--- client/src/Components/ServiceCard.jsx
+++ client/src/Components/ServiceCard.jsx
@@ -5,7 +5,7 @@ const ServiceCard = ({ name, description, image, isEven }) => {
 
     <div className={`service-card py-12 px-12 gap-8 flex min-h-[50vh] w-full md:flex-row flex-col justify-around items-center ${isEven ? 'bg-[#d0e3ff] text-primary md:flex-row' : 'bg-white text-primary md:flex-row-reverse'} `}>
       <div className="service-content flex-1 text-black flex flex-col justify-around items-start gap-3">
-        <h1 className={`service-title text-3xl font-semibold `}> {name} </h1>
+        <h1 className={`service-title text-3xl font-semibold capitalize `}> {name} </h1>
         <p dangerouslySetInnerHTML={{ __html: description }} className="service-description text-sm"/> 
       </div>
       <div className='image min-h-32 max-h-auto flex-1'>
