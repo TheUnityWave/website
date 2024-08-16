0e2817ff0cfd7a6b1e7633a0f72d04693105c32e
RchtDshr <rachita17dashore@gmail.com>
2024-08-16 20:47:10 +0530

    design

--- client/src/Components/ServiceCard.jsx
+++ client/src/Components/ServiceCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-const ServiceCard = ({ name, description, image, industry =[], isEven }) => {
+const ServiceCard = ({ name, description, image, industry = [], isEven }) => {
   return (
 
     <div className={`service-card py-12 px-12 gap-8 flex min-h-[70vh] w-full md:flex-row flex-col justify-around items-center ${isEven ? 'bg-[#d0e3ff] text-primary md:flex-row' : 'bg-white text-primary md:flex-row-reverse'} `}>
@@ -7,20 +7,26 @@ const ServiceCard = ({ name, description, image, industry =[], isEven }) => {
         <h1 className={`service-title text-3xl font-semibold capitalize `}> {name} </h1>
         <p dangerouslySetInnerHTML={{ __html: description }} className="service-description text-sm" />
 
+
         {industry.length > 0 && (
-        <div className="industry-icons mt-6 flex justify-evenly items-center gap-4">
-          {industry.map(({ name: industryName, iconPath }, index) => (
-            <div key={index} className="industry-item w-24 h-24">
-              <img 
-                src={iconPath} 
-                alt={`${industryName} icon`} 
-                className="industry-icon w-20 h-20 object-contain"
-              />
-              <div className="industry-name text-[0.75rem] text-center mt-2 text-wrap">{industryName}</div>
+          <div className="industry-icons md:mt-12">
+            <p className='font-bold'>
+              Industries where we serve:
+            </p>
+            <div className="flex md:justify-evenly items-center gap-4">
+              {industry.map(({ name: industryName, iconPath }, index) => (
+                <div key={index} className="industry-item md:w-24 md:h-24">
+                  <img
+                    src={iconPath}
+                    alt={`${industryName} icon`}
+                    className="industry-icon md:w-20 md:h-20 w-12 h-12 object-contain"
+                  />
+                  <div className="industry-name md:text-[0.75rem] text-[0.65rem] text-center mt-2 text-wrap">{industryName}</div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      )}
+          </div>
+        )}
 
       </div>
       <div className='image min-h-32 max-h-auto flex-1'>
