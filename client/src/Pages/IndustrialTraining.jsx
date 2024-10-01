71202ef8980314644fa75123e6534b4030f6d61b
Nikansha Saini <124757514+NikanshaS@users.noreply.github.com>
2024-10-01 12:48:18 +0530

    IndustrialTraining

---
+++ client/src/Pages/IndustrialTraining.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Course from '../Pages/Course';
+import Industry from '../Pages/Industry';
+import CourseInfo from '../Pages/CourseInfo';
+import CourseService from '../Pages/CourseService';
+import Faq from '../Pages/Faq';
+import Feedback from '../Pages/Feedback';
+
+const TrainingProgram = () => {
+    return (
+        <div>
+            <Industry />
+            <Course />
+            <CourseInfo />
+            <CourseService />
+            <Faq />
+            <Feedback />
+        </div>
+    );
+};
+export default TrainingProgram;
