da09d24fcbf1e6fd1dc7da7f13753d2ca22c95ae
ria-saini <riyasaini9319@gmail.com>
2024-10-02 11:58:23 +0530

    Services.jsx

--- client/src/Pages/Services.jsx
+++ client/src/Pages/Services.jsx
@@ -27,20 +27,12 @@ const imageMap = {
 
 const Services = () => {
   const titleRef = useRef(null);
-  const navigate = useNavigate();
+
 
   useGSAP(() => {
     animateTextWordByWord(titleRef.current, {duration : 2});
 
 }, []);
-  
-   const handleServiceClick = (serviceId) => {
-    if (serviceId === 'industrial-training-program') {
-      navigate('/service/industrial-training-program'); // Use the correct route path
-    } else {
-      navigate(`/service/${serviceId}`);
-    }
-  };
 
   return (
     <section className="py-12 bg-[#d0e3ff] " id='services'>
@@ -71,11 +63,10 @@ const Services = () => {
 
         <div className="mt-10 flex md:flex-row flex-col justify-around items-center ">
           {servicesJson.services.map((service) => (
-          <div
-
+           <Link 
+              to={`/service/${service.id}`} // Adjust the path as needed for your routing
               key={service.id}
               className="p-8 flex flex-col justify-center items-center transition-transform transform hover:scale-105 hover-effect"
-              onClick={() => handleServiceClick(service.id)}
             >
               {service.images[0] && (
                 <img
@@ -85,7 +76,7 @@ const Services = () => {
                 />
               )}
               <h3 className="mt-4 text-md font-medium text-center text-[#6B7280]">{service.name}</h3>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
