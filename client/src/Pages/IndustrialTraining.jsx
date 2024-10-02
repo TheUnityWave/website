c9d8fa585772824183e3ada9af927365cbce4727
ria-saini <riyasaini9319@gmail.com>
2024-10-02 11:35:39 +0530

    IndustrialTraining.jsx

--- client/src/Pages/IndustrialTraining.jsx
+++ client/src/Pages/IndustrialTraining.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Course from '../Pages/Course';
-import Industry from '../Pages/Industry';
-import CourseInfo from '../Pages/CourseInfo';
-import CourseService from '../Pages/CourseService';
-import Faq from '../Pages/Faq';
-import Feedback from '../Pages/Feedback';
+ import Industry from '../Pages/Industry';
+// import CourseInfo from '../Pages/CourseInfo';
+// import CourseService from '../Pages/CourseService';
+// import Faq from '../Pages/Faq';
+// import Feedback from '../Pages/Feedback';
 
 const TrainingProgram = () => {
     return (
         <div>
             <Industry />
-            <Course />
-            <CourseInfo />
+           <Course />
+{/*              <CourseInfo />
             <CourseService />
             <Faq />
-            <Feedback />
+            <Feedback /> */}
         </div>
     );
 };
