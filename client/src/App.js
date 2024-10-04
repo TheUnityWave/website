d6a36c27594d3e4ca22559e0526f04617c161388
ria-saini <riyasaini9319@gmail.com>
2024-10-04 13:27:04 +0530

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,8 +18,9 @@ import AboutUs from './Pages/AboutUs'
 import Blogs from './Pages/Blogs'
 import BlogDetail from './Pages/BlogDetail';
 import ReferAndEarn from './Pages/ReferAndEarn';
-import Jobs from './Pages/Jobs'
-import JobDetailPage from './Pages/JobDetail'
+import Jobs from './Pages/Jobs';
+import JobDetailPage from './Pages/JobDetail';
+import IndustrialTraining from './Pages/IndustrialTraining';
 
 
 function ScrollManager() {
@@ -99,6 +100,7 @@ function App() {
           <Route path="/admin/*" element={<AdminDashboard />} />
           <Route path="/employee/*" element={<EmployeeDashboard />} />
           <Route path="/refer-and-earn" element={<ReferAndEarn />} />
+          <Route path="/service/industrial-training-program" element={<IndustrialTraining />} />
         </Routes>
         <Footer />
       </div>
